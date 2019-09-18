9572206f19ce6b6736f40ac483ee15d92bf3d29b
ngaivoronski <ngaivoronski@gmail.com>
2019-09-18 12:01:51 -0400

    closure stretch

--- assignments/closure.js
+++ assignments/closure.js
@@ -22,14 +22,37 @@ const counterMaker = () => {
   //      NOTE: This `counter` function, being nested inside `counterMaker`,
   //      "closes over" the `count` variable. It can "see" it in the parent scope!
   // 3- Return the `counter` function.
+  let count = 0;
+  return function counter() {
+    return count += 1;
+  }
 };
 // Example usage: const myCounter = counterMaker();
 // myCounter(); // 1
 // myCounter(); // 2
+const myCounter = counterMaker();
+console.log(myCounter());
+console.log(myCounter());
 
 // ==== Challenge 3: Make `counterMaker` more sophisticated ====
 // It should have a `limit` parameter. Any counters we make with `counterMaker`
 // will refuse to go over the limit, and start back at 1.
+const counterMaker2 = (limit) => {
+  let count = 0;
+  return function counter() {
+    if (count >= limit) {
+      return count = 1;
+    }
+    else {
+      return count += 1;
+    }
+  }
+};
+const myCounter2 = counterMaker2(3);
+console.log(myCounter2());
+console.log(myCounter2());
+console.log(myCounter2());
+console.log(myCounter2());
 
 // ==== Challenge 4: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
